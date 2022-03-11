2364ca4e81452fdf6ff230f9c67cc3c8a0b3a164
JuliaRa411 <777.07j@gmail..com>
2022-03-11 18:16:32 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+ import { useState,useEffect } from 'react';
+ import './App.css';
 
 function App() {
+
+  const [data, setData] = useState( )
+  const [location, setLocation] = useState("london")
+
+
+
+  
+useEffect(() =>{
+  getWeather();
+},[location])
+
+
+ 
+ const getWeather = (async () =>{
+    const response = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=1bc7dff5c3a7aa89ab5cd008afeeb609`);
+    const data = await response.json()
+    console.log(data)
+ 
+  })
+ 
+ 
+const locationSearch =(e) =>{
+console.log(e.target.value) //  option to see all changes from users
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+    <form onSubmit>
+    <input className='search' placeholder='Enter Location' type="text" onChange={locationSearch} > 
+    </input>
+  </form>
+   </div>
+    
+    
   );
 }
 
