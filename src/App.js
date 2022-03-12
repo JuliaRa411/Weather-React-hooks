d503481024f1c5bfa837b8f03750962fd7b9db60
JuliaRa411 <777.07j@gmail..com>
2022-03-11 21:05:13 -0500

    commit-2

--- src/App.js
+++ src/App.js
@@ -1,41 +1,57 @@
  import { useState,useEffect } from 'react';
  import './App.css';
+import MyWeatherComponent from './MyWeatherComponent';
 
-function App() {
-
-  const [data, setData] = useState( )
-  const [location, setLocation] = useState("london")
-
-
+ function App() {
 
   
-useEffect(() =>{
-  getWeather();
-},[location])
+  
+  const [mySearch, setMySearch] = useState("") // доступ к enter
+  const [location, setLocation] = useState("london")
+  const [myWeather, setMyWeather] =useState([]) // отображаем погоду
+  
 
 
- 
- const getWeather = (async () =>{
-    const response = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=1bc7dff5c3a7aa89ab5cd008afeeb609`);
-    const data = await response.json()
-    console.log(data)
- 
-  })
- 
- 
-const locationSearch =(e) =>{
-console.log(e.target.value) //  option to see all changes from users
-}
+  useEffect(async() =>{
+   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=1bc7dff5c3a7aa89ab5cd008afeeb609`) ;
+   const data = await response.json();
+   console.log(data)
+   setMySearch(data)  // Пишет OBJECT в графе поиск
+      },[location])
+  
+   
+
+
+   const myWeatherSearch =(e)=>{
+    console.log(e.target.value)
+    
+     } 
+     
+
+    
+      
 
   return (
     <div className="container">
-    <form onSubmit>
-    <input className='search' placeholder='Enter Location' type="text" onChange={locationSearch} > 
+    <form>
+    <input className='search' placeholder='Enter location' type="text" onChange={myWeatherSearch} value={mySearch} > 
     </input>
+   
   </form>
-   </div>
-    
-    
+<div className='btn'> 
+  <button> Submit</button>
+ 
+  </div> 
+  
+   {mySearch.map(element => (
+    <MyWeatherComponent /> 
+     ))
+     }
+      
+ 
+  
+    </div> 
+  
   );
 }
 
---
+++ src/MyWeatherComponent.js
@@ -0,0 +1,11 @@
+function MyWeatherComponent(){
+
+    return(
+        <div>
+
+            <h1>TEST</h1>
+        </div>
+    )
+}
+
+export default MyWeatherComponent;
