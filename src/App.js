49552af6a90957f3fdef499a8275fcc02781c6ab
JuliaRa411 <777.07j@gmail..com>
2022-03-15 15:13:22 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@
  import './App.css';
  import Gsap from './Gsap';
  
- 
-  
    function App() {
    const [data, setData] = useState([])
    const [location, setLocation] = useState('')
@@ -15,7 +13,7 @@
    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=895284fb2d2c50a520ea537456963d9c`
    const iconUrl =`https://openweathermap.org/img/wn/${icon}@2x.png`
      
- const searchLocation = (event) => {
+   const searchLocation = (event) => {
      if (event.key === 'Enter') {
        axios.get(url).then((response) => {
          setData(response.data)
@@ -53,38 +51,33 @@
           <Gsap/>  
           
            <div className="location">
-            <p className="name"> {data.name} {sys}</p> 
-          
-            </div>
-           
+            <p className="name"> {data.name} </p> 
+             </div>
+             <div className="location">
+            <p className="name"> {sys}</p> 
+             </div>
             <div className="location"> </div>
                    <div className="temp">
              {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
              
-
            </div>
         
            <div className="description">
-             
-             {data.weather ? <p>{data.weather[0].main}</p> : null}
-           
-           </div>
-           <div className="description">
+           {data.weather ? <p>{data.weather[0].main}</p> : null}
+            </div>
 
+           <div className="description">
            <div className="weather-icon">
           {/*  Changing weather icons */ }
-          
-            <img src={iconUrl} alt ="" className='icon'/>  
-          
-    </div>
+                   <img src={iconUrl} alt ="" />  
+          </div>
                  
- </div>
+          </div>
          {data.name !== undefined &&
            <div className="bottom">
              <div className="feels">
              <p>Feels Like</p>
                {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°C</p> : null}
-             
              </div>
              <div className="humidity">
              <p>Humidity</p>
@@ -94,29 +87,17 @@
              <div className="wind">
              <p>Wind Speed</p>
               {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
-             
-
-
-             
+                          
             </div>
-             
-            </div>
+             </div>
          }
- 
-
- 
+        </div>
        </div>
-       
-
-
-     </div>
- 
-</div>
+ </div>
 </div>
  
    );
-   
-
+  
  }
  
  export default App;
